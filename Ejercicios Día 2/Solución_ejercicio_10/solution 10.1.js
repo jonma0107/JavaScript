e3e9fd74e62517713aa28a80d313801d915eaac7
Jonathan Mauricio Meza Bastidas <mbjonathan@misena.edu.co>
2021-09-09 00:39:11 -0500

    Solution

--- Ejercicios Día 2/Solución_ejercicio_10/solution 10.1.js
+++ Ejercicios Día 2/Solución_ejercicio_10/solution 10.1.js
@@ -4,4 +4,4 @@ const bmi = peso/altura**2
 if(bmi<18.5) console.log("Bajo de peso")
 	else if(bmi<24.9) console.log("Normal")
 		else if(bmi>25 && bmi<29.9) console.log("Sobrepeso")
-			else if(bmi>=30) console.log("Obeso")
+			else if(bmi>=30) console.log("Obeso!")
