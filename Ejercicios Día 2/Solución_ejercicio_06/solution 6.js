8940a4f81743f9199cc0ec3075ad78dd11e38984
Jonathan Mauricio Meza Bastidas <mbjonathan@misena.edu.co>
2021-09-08 23:57:40 -0500

    Solution

---
+++ Ejercicios Día 2/Solución_ejercicio_06/solution 6.js
@@ -0,0 +1,11 @@
+let num=parseFloat(prompt("Ingrese un número"));
+if (num<10){
+	console.log("El número es menor a 10");
+	alert("El número es menor a 10");
+} else if (num>10){
+	console.log("El número es mayor a 10");
+	alert("El número es menor a 10");
+} else{
+	console.log("El número es igual a 10");
+	alert("El número es menor a 10");
+}
