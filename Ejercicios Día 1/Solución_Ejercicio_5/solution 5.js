9430daf34978b53602251dc9c039bdc551ce8b69
Jonathan Mauricio Meza Bastidas <mbjonathan@misena.edu.co>
2021-09-08 21:10:24 -0500

    Solution

---
+++ Ejercicios Día 1/Solución_Ejercicio_5/solution 5.js
@@ -0,0 +1,5 @@
+let weight = parseFloat(prompt("Favor ingrese su peso corporal "));
+let height = parseFloat(prompt("Por favor ingrese su altura "));
+let bmi = weight/height**2;
+alert("Tu BMI es: "+bmi);
+console.log("Tu BMI es: "+bmi);
