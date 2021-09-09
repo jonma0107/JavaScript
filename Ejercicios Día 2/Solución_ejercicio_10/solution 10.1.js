4b186bbdce2803ad9b0efd2b93d51728c235c5dc
Jonathan Mauricio Meza Bastidas <mbjonathan@misena.edu.co>
2021-09-09 00:31:22 -0500

    Solution

---
+++ Ejercicios Día 2/Solución_ejercicio_10/solution 10.1.js
@@ -0,0 +1,7 @@
+const peso = parseFloat(prompt("Escriba su peso: "))
+const altura = parseFloat(prompt("Escriba su altura: "))
+const bmi = peso/altura**2
+if(bmi<18.5) console.log("Bajo de peso")
+	else if(bmi<24.9) console.log("Normal")
+		else if(bmi>25 && bmi<29.9) console.log("Sobrepeso")
+			else if(bmi>=30) console.log("Obeso")
