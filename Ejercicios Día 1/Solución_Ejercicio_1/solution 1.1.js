06698bb82e9287ac165f35836ca3d67b21ab2cee
Jonathan Mauricio Meza Bastidas <mbjonathan@misena.edu.co>
2021-09-08 20:47:43 -0500

    Solution

--- Ejercicios Día 1/Solución_Ejercicio_1/solution 1.1.js
+++ Ejercicios Día 1/Solución_Ejercicio_1/solution 1.1.js
@@ -1,6 +1,8 @@
 function hola(name){
 	return "Hola "+name+"!";
 }
-let name = prompt("escriba su nombre: ");
+let name = prompt("Escriba su nombre completo: ");
 hola(name);
+alert(name);
+document.write(name);
 console.log(hola(name));
