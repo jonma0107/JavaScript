938173376242f55ff90db0cdbaec093c9aa62a79
Jonathan Mauricio Meza Bastidas <mbjonathan@misena.edu.co>
2021-09-08 20:57:29 -0500

    Solution

---
+++ Ejercicios Día 1/Solución_Ejercicio_3/solution 3.js
@@ -0,0 +1,5 @@
+let num1=parseInt(prompt("Ingrese el primer número"));
+let num2=parseInt(prompt("Ingrese el segundo número"));
+suma=num1+num2;
+alert("La suma de los dos números que ingresó es: "+suma);
+console.log("La suma de los dos números que ingresó es: "+suma);
